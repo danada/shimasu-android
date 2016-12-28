870550b29b1e80f5f96dd32af855e3b18fde5e8b
Daniel De Sousa <dan.desousa@gmail.com>
2016-12-29 02:06:28 +0900

    changed activity type to radio selector dialog

--- app/src/main/java/enjoysmile/com/shimasu/ActivityAddActivity.java
+++ app/src/main/java/enjoysmile/com/shimasu/ActivityAddActivity.java
@@ -1,12 +1,19 @@
 package enjoysmile.com.shimasu;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ActivityAddActivity extends AppCompatActivity {
 
@@ -23,11 +30,36 @@ public class ActivityAddActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        /*
         activityType = (Spinner) findViewById(R.id.activity_type_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.activity_Types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         activityType.setAdapter(adapter);
+        */
+
+        View _selector = findViewById(R.id.activity_type_selector);
+        _selector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("THIS", "WAS CLICKED");
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityAddActivity.this);
+                final String[] _types = {"Reward", "Activity"};
+                builder.setTitle("Activity type")
+                    .setSingleChoiceItems(_types, -1, new DialogInterface
+                            .OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Group Name = " + _types[item], Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();// dismiss the alertbox after chose option
+
+                        }
+                    });
+
+                builder.create().show();
+            }
+        });
+
     }
 
     @Override
